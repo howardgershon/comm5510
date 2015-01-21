0ecb5de1665ab70cfeb4bb2267d74c9c9f803357
Evan D Young <evan.d.young@gmail.com>
2015-01-20 19:03:31 -0700

    working on index.html

---
+++ Assignments/Semantic HTML/js/kode.js
@@ -0,0 +1,7 @@
+var showMeTheMoney = function (e) {
+    if(document.getElementById(e).style.display=='none') {
+      document.getElementById(e).style.display = 'block-inline'
+    } else {
+      document.getElementById(e).sytle.display = 'none'
+    };
+}
