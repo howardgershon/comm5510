2d3389541c1fce16daed8360283f64559ad4f228
Evan D Young <evan.d.young@gmail.com>
2015-01-27 14:58:59 -0700

    working on Module 1

--- Assignments/Module1/js/kode.js
+++ Assignments/Module1/js/kode.js
@@ -14,4 +14,19 @@ window.addEventListener('load', function(){
   fam.addEventListener('mouseover', function(){
     info.style.display = 'inline';
   });
+  math.addEventListener('mouseover', function(){
+    info.style.display = 'inline';
+  });
+  code.addEventListener('mouseover', function(){
+    info.style.display = 'inline';
+  });
+  fam.addEventListener('mouseout', function(){
+    info.style.display = 'none';
+  });
+  math.addEventListener('mouseout', function(){
+    info.style.display = 'none';
+  });
+  code.addEventListener('mouseout', function(){
+    info.style.display = 'none';
+  });
 });
