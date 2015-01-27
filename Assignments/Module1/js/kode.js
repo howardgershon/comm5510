cbf76a34069c0786a1b20f56f8e06b0508042173
Evan D Young <evan.d.young@gmail.com>
2015-01-27 15:11:25 -0700

    working on Module 1

--- Assignments/Module1/js/kode.js
+++ Assignments/Module1/js/kode.js
@@ -12,21 +12,27 @@ var info = document.getElementById('info');
 
 window.addEventListener('load', function(){
   fam.addEventListener('mouseover', function(){
+    info.style.innerHTML = 'I have been studying mathematics for many years. I can remember how exicited I was when I first realized that you could multiply numbers and work outside of base-10. I received my BS in mathematics from the University of Utah in 2013 and will receive my MS in computational engineering and science from the University of Utah in 2015.';
     info.style.display = 'inline';
   });
   math.addEventListener('mouseover', function(){
+    info.style.innerHTML = '';
     info.style.display = 'inline';
   });
   code.addEventListener('mouseover', function(){
+    info.style.innerHTML = '';
     info.style.display = 'inline';
   });
   fam.addEventListener('mouseout', function(){
+    info.style.innerHTML = '';
     info.style.display = 'none';
   });
   math.addEventListener('mouseout', function(){
+    info.style.innerHTML = '';
     info.style.display = 'none';
   });
   code.addEventListener('mouseout', function(){
+    info.style.innerHTML = '';
     info.style.display = 'none';
   });
 });
