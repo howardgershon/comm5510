c05d3d1f2d4d3c6caa98321eee9c7aa19d7739e0
Evan D Young <evan.d.young@gmail.com>
2015-01-27 14:50:03 -0700

    working on Module 1

--- Assignments/Module1/js/kode.js
+++ Assignments/Module1/js/kode.js
@@ -1,4 +1,15 @@
+
 var content = document.getElementById('box');
+
 window.addEventListener('scroll', function(){
   content.style.opacity = (window.innerHeight-3*window.scrollY)/window.innerHeight;
 }, false);
+
+var fam = docuement.getElementById('fam');
+var math = docuement.getElementById('math');
+var code = docuement.getElementById('code');
+var info = docuement.getElementById('info');
+
+fam.addEventListener('mouseover'function(){
+  info.style.display = block;
+});
