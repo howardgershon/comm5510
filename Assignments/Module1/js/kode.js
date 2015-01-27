c1dce583f6e985010d207f7b5e0c6dec91985572
Evan D Young <evan.d.young@gmail.com>
2015-01-27 15:47:02 -0700

    working on Module 1

--- Assignments/Module1/js/kode.js
+++ Assignments/Module1/js/kode.js
@@ -12,15 +12,15 @@ var info = document.getElementById('info');
 
 window.addEventListener('load', function(){
   fam.addEventListener('mouseover', function(){
-    info.innerHTML = 'I have been studying mathematics for many years. I can remember how exicited I was when I first realized that you could multiply numbers and work outside of base-10. I received my BS in mathematics from the University of Utah in 2013 and will receive my MS in computational engineering and science from the University of Utah in 2015.';
+    info.innerHTML = 'My family is the most important aspect of my life. I am married with four wonderful children: our three sons Kyle (6), Aiden (4), and Owen (&lt;&lt;1), and our daughter Charlie (1)';
     info.style.display = 'inline-block';
   });
   math.addEventListener('mouseover', function(){
-    info.innerHTML = '';
+    info.innerHTML = 'I have been studying mathematics for many years. I can remember how exicited I was when I first realized that you could multiply numbers and work outside of base-10. I received my BS in mathematics from the University of Utah in 2013 and will receive my MS in computational engineering and science from the University of Utah in 2015.';
     info.style.display = 'inline-block';
   });
   code.addEventListener('mouseover', function(){
-    info.innerHTML = '';
+    info.innerHTML = 'From the time I used the C-prompt to play Wolfenstein 3D until now computers have been an intergral part of my life. Currenlty, I use computers for mathematical and statistical computing (in R) and web development (HTML5, CSS3, and JavaScript)';
     info.style.display = 'inline-block';
   });
   fam.addEventListener('mouseout', function(){
