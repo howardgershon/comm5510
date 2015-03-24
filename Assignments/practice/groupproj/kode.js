19545db88f2e0040faca49166022a9a02518fbee
Evan D Young <evan.d.young@gmail.com>
2015-03-24 16:08:37 -0600

    adding test

--- Assignments/practice/groupproj/kode.js
+++ Assignments/practice/groupproj/kode.js
@@ -1,6 +1,6 @@
 var showMeTheMoney = function (e) {
     if(document.getElementById(e).style.display=='none' || document.getElementById(e).style.display == "") {
-      document.getElementById(e).style.display = 'inline'
+      document.getElementById(e).style.display = 'inline-block'
     } else {
       document.getElementById(e).style.display = 'none'
     };
